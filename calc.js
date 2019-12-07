cd6d21b59f69a7e4125d4c93799081ea9ebf3d11
Alok Dey <deyniva@gmail.com>
2019-12-07 10:59:36 -0500

    test code push

--- calc.js
+++ calc.js
@@ -8,7 +8,9 @@ function dis(val)
 function solve() 
 { 
     let x = document.getElementById("result").value 
+   
     let y = eval(x) 
+    
     document.getElementById("result").value = y 
 } 
 
